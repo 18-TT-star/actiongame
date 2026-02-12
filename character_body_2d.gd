fa35e33666daa15ebaaae13bedc026d515936be5
18-TT-star <takayukitooya@gmail.com>
2026-02-12 20:48:16 +0900

    攻撃実装前

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -23,8 +23,8 @@ func _physics_process(delta: float) -> void:
 	var dir := Input.get_axis("ui_left", "ui_right")
 	velocity.x = dir * SPEED
 
-	# ジャンプ（ui_accept はデフォルトでSpace/Enter）
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# ジャンプ（スペースキーのみ）
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
